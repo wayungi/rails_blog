9c4a572fda286a84713c68a040deff030361b7d2
wayungi <fwayungi@gmail.com>
2022-10-20 21:31:54 +0300

    UPDATE: user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :comments
+  has_many :posts
+  has_many :likes
 
   def get3posts
     posts.order(:created_at).first(3)
   end
 end
+
+
+# has_many :posts, foreign_key: 'author_id'
+# has_many :comments, foreign_key: 'author_id'
+# has_many :likes, foreign_key: 'author_id'
