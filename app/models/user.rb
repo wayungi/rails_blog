a3b96164d7bb2a12e508d67193913d60174bd4e2
wayungi <fwayungi@gmail.com>
2022-10-20 21:35:08 +0300

    Fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   end
 end
 
-
 # has_many :posts, foreign_key: 'author_id'
 # has_many :comments, foreign_key: 'author_id'
 # has_many :likes, foreign_key: 'author_id'
