1f0d800e509ca0e9c97639fe702b53641b6ad908
wayungi <fwayungi@gmail.com>
2022-10-20 19:02:02 +0300

    Add getCommnets methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,20 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :commnets
+    belongs_to :user  # counter_cache: :postsCounter
+    has_many :comments # counter_cache: :commentsCounter
     has_many :likes
+
+    # A method which returns the 5 most recent comments for a given post.
+    def getcomments
+        comments.order(:created_at).first(5)
+    end
+
+    # A method that updates the posts counter for a user.
+    def updatecounter
+        # user.postsCounter +=1
+        # user.save
+    end
+
+
+    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :comments
-    has_many :posts
+    has_many :posts, counter_cache: :commentsCounter
     has_many :likes
+
+    def get3posts
+        posts.order(:created_at).first(3)
+    end
 end
